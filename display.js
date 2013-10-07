de62845cb5b448669e6a95cad57a412abfd2dc8c
Vadim Galkin <vadimiztveri@yandex.ru>
2013-10-07 14:18:29 +0400

    alpha

--- display.js
+++ display.js
@@ -6,11 +6,10 @@
 *
 * Функция не получает данные в себя, берет значения из конфигурации color, и не возвращает данные
 */
-var display_it = function(){
+var display_color = function(){
   var string_color = color.red + color.green + color.blue;
-  document.getElementById('rgb').value = get_hex();
-  document.getElementById('color').style.background = "#" + get_hex();
-  console
+  document.getElementById('rgb').value = string_color;
+  document.getElementById('color').style.background = "#" + string_color;
 }
 
 /**
@@ -21,7 +20,7 @@ var display_it = function(){
 *
 * Не получает и не возвращает значения
 */
-var move_fider = function() {
+var chance_pointer_position = function() {
   document.getElementById('fider').style.left = hex_to_dec(color.red) + "px";
   document.getElementById('draggable').style.top = hex_to_dec(color.green) + "px";
   document.getElementById('draggable').style.left = hex_to_dec(color.blue) + "px";
@@ -39,4 +38,21 @@ var move_fider = function() {
 var change_green_blue_for_red = function(red){
   var opacity = red / 255;
   document.getElementById('square-red').style.opacity = opacity;
+}
+
+/**
+* Отображает сообщение об ошибке
+*
+* @example
+* display_error_massage(1);
+*
+* @param {Number} number Номер ошибки (0, 1, 2) (например: 1)
+* Номер 0 означает, что вводимые данные прошли валидацию и функция удаляет сообщение об ошибке.
+* Не возвращает данные
+*/
+var display_error_massage = function(number) {
+  var text = "";
+  if (number === 1) {text = "Введите 6 или 3 символа, например: ffffff, ссс.";}
+  if (number === 2) {text = "Символы должны быть либо цифрами, либо буквам латинского алфавита от A до F";}
+  document.getElementById('error').innerHTML = text;
 }
--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 /**
-* Обращение к JQ, указание на DIV id="draggable" как на объект, который будет перетаскиваться.
+* Обращение к JQ, указание на DIV с id="draggable" как на объект, который будет перетаскиваться.
 * При перемещении объекта вызывает функцию get_value_square_field() и передает ей данные о координатах слева и сверху относительно родителя
 */
 $(function(){
@@ -9,14 +9,14 @@ $(function(){
     opacity: 0.6,
     refreshPosition: true,
     drag: function(event, ui) {
-      get_value_square_field(ui.position.left, ui.position.top)
+      get_value_green_blue_field(ui.position.left, ui.position.top)
     }
   });
 });
 
 /**
 * Обращение к JQ, указание на DIV id="fider" как на объект, который будет перетаскиваться.
-* При перемещении объекта вызывает функцию get_value_square_field() и передает ей данные о координатах слева от родителского блока
+* При перемещении объекта вызывает функцию get_value_rectangular_field() и передает ей данные о координатах слева от родителского блока
 */
 $(function(){
   $("#fider").draggable({
@@ -26,7 +26,7 @@ $(function(){
     refreshPosition: true,
     axis: "x",
     drag: function(event, ui) {
-      get_value_rectangular_field(ui.position.left)
+      get_value_red_field(ui.position.left)
     }
   });
 });
@@ -41,28 +41,15 @@ var color = {
 }
 
 /**
-* При вызове возвращает строку текущего цвета в HEX
-*
-* @example
-* get_hex();
-*
-* Не получает данные, берет их из конфигурации
-* @return {String} строка в HEX (например: FF00SS)
-*/
-var get_hex = function(){
-  return color.red + color.green + color.blue;
-}
-
-/**
 * Получает значение градации цвета от 0 до 255, возвращает в HEX
 *
 * @example
-* get_dec_to_hex(255); // return "FF"
+* chance_dec_to_hex(255); // return "FF"
 *
 * @param {Number} number градацию цвета от 0 до 255 (например: 255)
 * @return {String} строка в HEX (например: FF)
 */
- var get_dec_to_hex = function(number){
+ var chance_dec_to_hex = function(number){
  var hex = Number(number).toString(16);
  if (number < 16){
    hex = "0" + hex;
@@ -91,28 +78,52 @@ var hex_to_dec = function(hex){
 *
 * @param {String} color_hex цвет в RGB (например, "002244")
 * Не возвращает данные, а вызывает функции отображения цвета
+* Если строка состоит не из тех символов, или символов не 3 и не 6, то запускает функцию отображения ошибки
 */
 var get_new_color_hex = function(color_hex){
-  color.red = color_hex[0] + color_hex[1];
-  color.green = color_hex[2] + color_hex[3];
-  color.blue = color_hex[4] + color_hex[5];
-  display_it();
-  move_fider();
+  var reg = /^[0-9a-fA-F]$/,
+      error = 0;
+  for (i = 0;i < color_hex.length;i++){
+    if (!reg.test(color_hex[i])) {error = 2;}
+  }
+  if (error === 2) {
+    display_error_massage(2);
+  } else {
+    if (color_hex.length === 6) {
+      color.red = color_hex.substring(0, 2);
+      color.green = color_hex.substring(2, 4);
+      color.blue = color_hex.substring(4);
+      display_color();
+      chance_pointer_position();
+      display_error_massage(0);
+    } else {
+      if (color_hex.length === 3){
+        color.red = color_hex[0] + color_hex[0];
+        color.green = color_hex[1] + color_hex[1];
+        color.blue = color_hex[2] + color_hex[2];
+        display_color();
+        chance_pointer_position();
+        display_error_massage(0);
+      } else {
+        display_error_massage(1);
+      }
+    }
+  }
 }
 
 /**
 * Получает значения blue и green, записывает в текущий цвет, вызывает функцию отображения
 *
 * @example
-* get_value_square_field("22", "44");
+* get_value_green_blue_field("22", "44");
 *
 * @param {String} blue, green градации каждого цвета от 0 до 255 в HEX ("22", "44")
 * Не возвращает данные, а вызывает функции отображения цвета
 */
-var get_value_square_field = function(blue, green){
-  color.green = get_dec_to_hex(green);
-  color.blue = get_dec_to_hex(blue);
-  display_it();
+var get_value_green_blue_field = function(blue, green){
+  color.green = chance_dec_to_hex(green);
+  color.blue = chance_dec_to_hex(blue);
+  display_color();
 }
 
 /**
@@ -124,8 +135,8 @@ var get_value_square_field = function(blue, green){
 * @param {Number} red градацию цвета от 0 до 255 (например: 125)
 * Не возвращает данные, а вызывает функции отображения цветов
 */
-var get_value_rectangular_field = function(red){
-  color.red = get_dec_to_hex(red);
-  display_it();
+var get_value_red_field = function(red){
+  color.red = chance_dec_to_hex(red);
+  display_color();
   change_green_blue_for_red(red);
 }
