9d75ffe09073bb73ec41aae9958d673a5720feec
vadimiztveri <vadimiztveri@yandex.ru>
2013-10-23 15:52:16 +0400

    with one global

--- color_output.js
+++ color_output.js
@@ -1,20 +1,13 @@
 /**
- * Конструктор данных для вывода
- *
- * @example
- * new ColorOutput([0, 125, 255]);
- *
+ * Конструктор данных для вывода цвета
  * @param {Area} rgba, три числа от 0 до 255, градации цветов в RGB (например: [0, 125, 255])
  * @constructor
  */
 var ColorOutput = function (hex_color) {
   this.hex_color = hex_color;
-}
+};
 
-/**
- * Выводит данные
- */
 ColorOutput.prototype.redraw = function() {
   document.getElementById('rgb').value = this.hex_color;
-  document.getElementById('color').style.background = "#" + this.hex_color;
-}
+  document.getElementById('color-ui').style.background = "#" + this.hex_color;
+};
